23c7f1ee5e8a47efafb8be8b80ca00396603f735
Tuyen Truong <tuyen@hooverfam.net>
2021-08-05 16:55:23 -0400

    adding talk template

--- app.py
+++ app.py
@@ -14,7 +14,18 @@ def hello():
 def new_talk():
     #return "An ALA conference title mashup will be here!"    
     talk_title = talk_mashup_bot.generateTitle()
-    return talk_title
+    #return talk_title
+    return render_template('new_talk.html', title=talk_title)
 
+@app.route('/talk', methods=['POST', 'GET'])
+def talk():
+    if request.method == 'GET':
+        return redirect(url_for('new_talk'))
+
+    talk_title = request.form['form-title']
+    talk_description = request.form['form-description']
+
+    return render_template('talk.html', title=talk_title, description=talk_description)
+    
 if __name__ == '__main__':
     app.run(debug=True)
